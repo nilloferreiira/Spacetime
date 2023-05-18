5a128264b835ab2a5ba310e7a4bef3ef2a38c2b1
Danillo <danillodana@outlook.com>
2023-05-18 15:43:48 -0300

    commitWeb

---
+++ web/src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import { getUser } from '@/lib/auth'
+import Image from 'next/image'
+
+export function Profile() {
+  const { name, avatarUrl } = getUser()
+
+  return (
+    <div
+      //   href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
+      className="flex items-center gap-3 text-left"
+    >
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
+
+      <p className="text-sm max-w-[155px] leading-snug">
+        {name}
+        <a href="" className="block text-red-400 hover:text-red-300">
+          Quero sair
+        </a>
+      </p>
+    </div>
+  )
+}
